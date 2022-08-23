aeb1501509dfda8e39391d49748b333f58b9923f
TRMO <tromortensen@hotmail.com>
2022-08-23 21:27:28 +0200

    Added inter project dependency. I Guess I did something to UserFileDao.

--- FileData/DAOs/UserFileDAO.cs
+++ FileData/DAOs/UserFileDAO.cs
@@ -1,6 +1,35 @@
-﻿namespace FileData.DAOs;
+﻿using Domain.DaoInterfaces;
+using Domain.Models;
 
-public class UserFileDAO
+namespace FileData.DAOs;
+
+public class UserFileDAO : IUserDao
 {
-    
+    private readonly FileContext context;
+
+    public UserFileDAO(FileContext context)
+    {
+        this.context = context;
+    }
+
+    public Task<User> Create(User user)
+    {
+        int nextId = context.Users.Max(u => u.Id);
+        nextId++;
+
+        user.Id = nextId;
+
+        context.Users.Add(user);
+        context.SaveChanges();
+
+        return Task.FromResult(user);
+    }
+
+    public Task<User?> GetByUsername(string userName)
+    {
+        User? existing = context.Users.FirstOrDefault(u =>
+            u.UserName.Equals(userName, StringComparison.OrdinalIgnoreCase)
+        );
+        return Task.FromResult(existing);
+    }
 }
