7005e4085355e39cc92d360056ec4084dcd67ed3
TRMO <tromortensen@hotmail.com>
2022-08-23 20:54:32 +0200

    Cleaned up. Added controller

---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Domain.DTOs;
+using Domain.LogicInterfaces;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserLogic userLogic;
+
+    public UsersController(IUserLogic userLogic)
+    {
+        this.userLogic = userLogic;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<User>> Create(UserCreationDto dto)
+    {
+        try
+        {
+            User user = await userLogic.Create(dto);
+            return Created($"/users/{user.Id}", user);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+}
