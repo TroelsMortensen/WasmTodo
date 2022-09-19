c5bc3a7959b8fc44d8d7a1139bb664f89836ea4b
TRMO <tromortensen@hotmail.com>
2022-09-19 21:39:52 +0200

    View all users probably done

--- HttpClients/ClientInterfaces/IUserService.cs
+++ HttpClients/ClientInterfaces/IUserService.cs
@@ -6,4 +6,5 @@ namespace HttpClients.ClientInterfaces;
 public interface IUserService
 {
     Task<User> Create(UserCreationDto dto);
+    Task<IEnumerable<User>> GetUsers(string? usernameContains = null);
 }
--- HttpClients/Implementations/UserHttpClient.cs
+++ HttpClients/Implementations/UserHttpClient.cs
@@ -24,7 +24,32 @@ public class UserHttpClient : IUserService
             throw new Exception(result);
         }
 
-        User user = JsonSerializer.Deserialize<User>(result)!;
+        User user = JsonSerializer.Deserialize<User>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
         return user;
+    }
+
+    public async Task<IEnumerable<User>> GetUsers(string? usernameContains = null)
+    {
+        string uri = "/users";
+        if (!string.IsNullOrEmpty(usernameContains))
+        {
+            uri += $"?username={usernameContains}";
+        }
+        HttpResponseMessage response = await client.GetAsync(uri);
+        string result = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(result);
+        }
+
+        Console.WriteLine(result);
+        IEnumerable<User> users = JsonSerializer.Deserialize<IEnumerable<User>>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return users;
     }
 }
