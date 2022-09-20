7110d3c84f2dcbed9a1940297fc4e22bd376a785
TRMO <tromortensen@hotmail.com>
2022-09-20 21:59:04 +0200

    Working on todo service

--- HttpClients/ClientInterfaces/ITodoService.cs
+++ HttpClients/ClientInterfaces/ITodoService.cs
@@ -1,8 +1,10 @@
 ﻿using Domain.DTOs;
+using Domain.Models;
 
 namespace HttpClients.ClientInterfaces;
 
 public interface ITodoService
 {
     Task CreateAsync(TodoCreationDto dto);
+    Task<ICollection<Todo>> GetAsync(string? userName, int? userId, bool? completedStatus, string? titleContains);
 }
--- HttpClients/Implementations/TodoHttpClient.cs
+++ HttpClients/Implementations/TodoHttpClient.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Http.Json;
 using System.Text.Json;
 using Domain.DTOs;
+using Domain.Models;
 using HttpClients.ClientInterfaces;
 
 namespace HttpClients.Implementations;
@@ -22,5 +23,49 @@ public class TodoHttpClient : ITodoService
             string content = await response.Content.ReadAsStringAsync();
             throw new Exception(content);
         }
+    }
+
+    public async Task<ICollection<Todo>> GetAsync(string? userName, int? userId, bool? completedStatus, string? titleContains)
+    {
+        string query = ConstructQuery(userName, userId, completedStatus, titleContains);
+
+        HttpResponseMessage response = await client.GetAsync("/todos" + query);
+        string content = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(content);
+        }
+
+        ICollection<Todo> todos = JsonSerializer.Deserialize<ICollection<Todo>>(content)!;
+        return todos;
+    }
+
+    private static string ConstructQuery(string? userName, int? userId, bool? completedStatus, string? titleContains)
+    {
+        string query = "";
+        if (userName != null)
+        {
+            query += $"?username={userName}";
+        }
+
+        if (userId != null)
+        {
+            query += string.IsNullOrEmpty(query) ? "?" : "&";
+            query += $"userid={userId}";
+        }
+
+        if (completedStatus != null)
+        {
+            query += string.IsNullOrEmpty(query) ? "?" : "&";
+            query += $"completedstatus={completedStatus}";
+        }
+
+        if (titleContains != null)
+        {
+            query += string.IsNullOrEmpty(query) ? "?" : "&";
+            query += $"titlecontains={titleContains}";
+        }
+
+        return query;
     }
 }
