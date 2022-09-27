cf248311e1cd259b83f2817195c8169f44795bb0
Troels Mortensen <tromortensen@hotmail.com>
2022-09-27 13:47:38 +0200

    example complete

--- BlazorWASM/Program.cs
+++ BlazorWASM/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorWASM;
+using BlazorWASM.StateContainers;
 using HttpClients.ClientInterfaces;
 using HttpClients.Implementations;
 
@@ -11,6 +12,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7093") });
 builder.Services.AddScoped<IUserService, UserHttpClient>();
 builder.Services.AddScoped<ITodoService, TodoHttpClient>();
-
+builder.Services.AddScoped<CounterStateContainer>();
 
 await builder.Build().RunAsync();
---
+++ BlazorWASM/StateContainers/CounterStateContainer.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorWASM.StateContainers;
+
+public class CounterStateContainer
+{
+    public Action<int> OnChange { get; set; }
+
+    private int count = 0;
+
+    public void Increment()
+    {
+        count++;
+        OnChange?.Invoke(count);
+    }
+}
