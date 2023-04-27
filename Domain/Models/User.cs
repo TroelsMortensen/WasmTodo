27fe967861d9d489e257be8b49604c57c59aea22
Troels Mortensen <tromortensen@hotmail.com>
2023-04-27 12:08:46 +0200

    Removed property to avoid ChangeTracker clash

--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -6,6 +6,6 @@ public class User
 {
     public int Id { get; set; }
     public string UserName { get; set; }
-    [JsonIgnore]
-    public ICollection<Todo> Todos { get; set; }
+    // [JsonIgnore]
+    // public ICollection<Todo> Todos { get; set; }
 }
